6cddb06af8043861f12c7eec2d382548b786026e
Victor Campos <campos.viictor@gmail.com>
2020-11-06 12:44:18 -0300

    creating tours project

---
+++ tours/src/components/Tours.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>ours tours</h2>
+        <div className="underline" />
+      </div>
+      <div>
+        {tours.map(tour => (
+          <Tour 
+            key={tour.id} 
+            {...tour} 
+            removeTour={removeTour} 
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
