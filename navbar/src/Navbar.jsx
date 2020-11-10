6cfd3e822f13555391cb8b251ce8ceb65a5a1d80
Victor Campos <campos.viictor@gmail.com>
2020-11-10 15:37:35 -0300

    creating dynamic navbar project

---
+++ navbar/src/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+
+import logo from './logo.svg';
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if(showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  
+  
+  }, [showLinks]);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo"/>
+          <button className="nav-toggle" 
+            onClick={() => 
+              setShowLinks(!showLinks)
+            }
+          >
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map(link => (
+              <li key={link.id}>
+                <a href={link.url}>{link.text}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ul className="social-icons">
+            {social.map(item => (
+              <li key={item.id}>
+                <a href={item.url}>{item.icon}</a>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
